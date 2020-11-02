fbd049c5722e35d333964e10655719d94e3904e4
Abhinav1281 <abhisione2@gmail.com>
2020-11-02 12:03:21 +0530

    Added Notifications

--- app/src/main/java/com/example/noted/App.java
+++ app/src/main/java/com/example/noted/App.java
@@ -3,6 +3,7 @@ package com.example.noted;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseInstallation;
 
 public class App extends Application {
 
@@ -13,5 +14,8 @@ public class App extends Application {
                 .applicationId("I9fUDTKFWaP3qfbiDGcOBP6ZRN6t7BsgauvUwT9n")
                 .clientKey("YjScY8HH30QrAqz3CmGTLWB0LnpMxfr6lSNsD4g7").server("https://parseapi.back4app.com/")
                 .build());
+        ParseInstallation installation = ParseInstallation.getCurrentInstallation();
+        installation.put("GCMSenderId","705344074314");
+        installation.saveInBackground();
     }
 }
